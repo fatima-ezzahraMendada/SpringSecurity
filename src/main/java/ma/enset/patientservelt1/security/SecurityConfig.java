6eed196513788e108b9a797c173651dc5645f9e5
fatima ezzahra mendada <fatimaezzahra.mendada@gmail.com>
2023-07-16 09:59:03 +0200

    first commit

--- src/main/java/ma/enset/patientservelt1/security/SecurityConfig.java
+++ src/main/java/ma/enset/patientservelt1/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package ma.enset.patientservelt1.security;
 
-import org.h2.jdbcx.JdbcDataSource;
+//import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
